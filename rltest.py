82a830b0499580e83eca4e6b1c626aa6198e0b1b
Cameron Motevasselani <ckmoteva@ucdavis.edu>
2012-12-27 00:16:07 -0800

    fixed movement issues

--- rltest.py
+++ rltest.py
@@ -8,8 +8,9 @@ LIMIT_FPS = 20
 
 def handle_keys():
     global playerx, playery
-
     key = libtcod.console_wait_for_keypress(True)
+    if key.pressed == False:
+        return False
     if key.vk == libtcod.KEY_ENTER and key.lalt:
         #Alt+Enter: toggle fullscreen
         libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
@@ -25,7 +26,6 @@ def handle_keys():
             playerx -= 1
         elif key.c == ord('l'):
             playerx += 1
-
 
 libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'rltest', False)

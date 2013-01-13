e3a756449eefdcc3372b7caa8362b40840735e0a
Cameron Motevasselani <ckmoteva@ucdavis.edu>
2013-01-12 17:28:39 -0800

    added dungeon generation

--- rltest.py
+++ rltest.py
@@ -8,6 +8,9 @@ LIMIT_FPS = 20
 
 MAP_WIDTH = 80
 MAP_HEIGHT = 45 
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
 
 color_dark_wall = libtcod.Color(0, 0, 100)
 color_dark_ground = libtcod.Color(50, 50, 150)
@@ -19,9 +22,47 @@ class Tile:
         self.blocked = blocked
 
         #by default, if a tile is blocked, also blocks sight
-        if block_sight is None: block_sight = blocked
+        if block_sight is None: block_sight = blocked           #must specify block_sight is false to get transparent, impassable tiles 
         self.block_sight = block_sight
 
+class Rect:
+    #a rectangl on the map, used for a room (usually)
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
+
+    def center(self):
+        #returns center of room
+        center_x = (self.x1 + self.x2) / 2
+        center_y = (self.y1 + self.y2) / 2
+        return (center_x, center_y)
+
+    def intersect(self, other):
+        #returns true if this rectangle intersects with another one
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+                self.y1 <= other.y2 and self.y2 >= other.y1)
+
+def create_room(room):
+    global map
+    #go through the tiles in the rectangle and make them passable
+    for x in range(room.x1 + 1, room.x2):
+        for y in range(room.y1 + 1, room.y2):
+            map[x][y].blocked = False
+            map[x][y].block_sight = False
+
+def create_h_tunnel(x1, x2, y):
+    global map
+    for x in range(min(x1, x2), max(x1, x2) + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+
+def create_v_tunnel(y1, y2, x):
+    global map
+    for y in range(min(y1, y2), max(y1, y2) + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
 
 def handle_keys():
 
@@ -48,7 +89,7 @@ def handle_keys():
 
 class Object: 
     #generic object class: player, monsters, items, etc.
-    #it's (the object) is always represented by a char on the screen
+    #the object should always be represented by a char on the screen
 
     def __init__(self, x, y, char, color):
         self.x = x
@@ -75,14 +116,64 @@ def make_map():
     global map
 
     #fill map with "unblocked" tiles
-    map = [[ Tile(False)
+    map = [[ Tile(True)
         for y in range(MAP_HEIGHT) ]
             for x in range(MAP_WIDTH) ]
 
-    map[30][22].blocked = True
-    map[30][22].block_sight = True
-    map[50][22].blocked = True
-    map[50][22].block_sight = True
+    rooms = []
+    num_rooms = 0
+    for r in range(MAX_ROOMS):
+        #random width and height
+        w = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        h = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        x = libtcod.random_get_int(0, 0, MAP_WIDTH - w - 1)
+        y = libtcod.random_get_int(0, 0, MAP_HEIGHT -h - 1)
+
+        new_room = Rect(x, y, w, h)
+
+        #run through the other rooms to see if they overlap
+        failed = False
+        for other_room in rooms:
+            if new_room.intersect(other_room):
+                failed = True
+                break
+        if not failed:
+            #no intersections, create the room!
+            create_room(new_room)
+
+            #get the center coordinates of the new room
+            (new_x, new_y) = new_room.center()
+
+            #print the room number (debugging)
+            #prints characters rather than numbers, #rooms may be > 10
+            room_no = Object(new_x, new_y, chr(65+num_rooms), libtcod.white)
+            objects.insert(0, room_no) #draw early so monsters are drawn on top
+
+            if num_rooms == 0:
+                #this is the first room, where the player starts at
+                player.x = new_x
+                player.y = new_y
+            else:
+                #all rooms after the first
+                #connect it to the previous room with a tunnel
+
+                #center coordinates of the previous room
+                (prev_x, prev_y) = rooms[num_rooms - 1].center()
+
+                #flip a coin
+                if libtcod.random_get_int(0, 0, 1) == 1:
+                    #move horizontally first, then vertically
+                    create_h_tunnel(prev_x, new_x, prev_y)
+                    create_v_tunnel(prev_y, new_y, new_x)
+                else: 
+                    #move vertically first, then horizontally
+                    create_v_tunnel(prev_y, new_y, prev_x)
+                    create_h_tunnel(prev_x, new_x, new_y)
+
+            #finally, append the new room to the list
+            rooms.append(new_room)
+            num_rooms += 1
+
     
 def render_all():
     #draw all objects in the list
@@ -109,6 +200,7 @@ player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
 npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
 objects = [npc, player]
 make_map()
+
 
 ###########################################
 #main loop

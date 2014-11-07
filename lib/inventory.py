71453fece4a5a7f2003cfe76b92089bf493393fd
Cameron Motevasselani <cmotevasselani@proofpoint.com>
2014-11-06 23:00:41 -0800

    added drop functionality

--- lib/inventory.py
+++ lib/inventory.py
@@ -1,5 +1,6 @@
 __author__ = 'cmotevasselani'
 
+from lib import libtcodpy as libtcod
 from lib.consoles.menu import Menu
 
 INVENTORY_WIDTH = 50
@@ -7,23 +8,33 @@ INVENTORY_WIDTH = 50
 
 class Inventory:
 
-    def __init__(self, status_panel, objects):
+    def __init__(self, status_panel, objects, player):
         self.status_panel = status_panel
         self.inventory = []
         self.objects = objects
         self.menu = Menu()
+        self.player = player
 
     def get_status_panel(self):
         return self.status_panel
 
-    def inventory_menu(self, header, con, screen_width, screen_height):
+    def inventory_menu(self, header, util):
         #show a menu with each item of the inventory as an option
         if len(self.inventory) == 0:
             options = ['Inventory is empty.']
         else:
             options = [item.name for item in self.inventory]
 
-        index = self.menu.display_menu(header, options, INVENTORY_WIDTH, con, screen_width, screen_height)
+        index = self.menu.display_menu(header, options, INVENTORY_WIDTH, util.con, util.SCREEN_WIDTH, util.SCREEN_HEIGHT)
 
         if index is None or len(self.inventory) == 0: return None;
         return self.inventory[index].item
+
+    def drop(self, object):
+        self.objects.append(object.owner)
+        self.inventory.remove(object.owner)
+        object.owner.x == self.player.x
+        object.owner.y == self.player.y
+        self.status_panel.message('You drop a ' + object.owner.name + '.', libtcod.turquoise)
+
+

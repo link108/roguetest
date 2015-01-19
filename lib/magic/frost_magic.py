fa3e0037ced095ed2cfa644910b7e9f6767a53a4
Cameron Motevasselani <cmotevasselani@proofpoint.com>
2015-01-19 15:29:22 -0800

    working on magic system, this commit might not be playable

---
+++ lib/magic/frost_magic.py
@@ -0,0 +1,19 @@
+__author__ = 'cmotevasselani'
+
+from lib.random_libs import libtcodpy as libtcod
+from lib.utility_functions.util import Util
+from lib.constants.constants import Constants
+
+class FrostMagic:
+
+    @staticmethod
+    def frost_shock(state):
+        monster = Util.target_monster(state, Constants.FROST_SHOCK_RANGE)
+        if monster is None:
+            state.status_panel.message('No enemy is close enough or none selected', libtcod.red)
+            return Constants.CANCELLED
+        state.status_panel.message('You Frost Shock: ' + monster.name + '! The damage done is '
+                                   + str(Constants.FROST_SHOCK_DAMAGE) + ' hp.', libtcod.light_blue)
+        monster.fighter.take_damage(Constants.FROST_SHOCK_DAMAGE, state)
+
+
---
+++ lib/magic/spell_inventory.py
@@ -0,0 +1,44 @@
+from lib.random_libs import libtcodpy as libtcod
+
+__author__ = 'cmotevasselani'
+
+from lib.consoles.menu import Menu
+from lib.constants.constants import Constants
+from lib.utility_functions.util import Util
+
+class SpellInventory:
+
+    def __init__(self, state):
+        self.status_panel = state.status_panel
+        self.menu = Menu()
+        self.player = state.player
+        self.spells = state.player.caster.spells
+
+    def spell_menu(self, header, state):
+        #show a menu with each item of the inventory as an option
+        if len(self.spells) == 0:
+            options = ['You have no spells.']
+        else:
+            options = []
+            for spell_name in state.player.caster.spells:
+                spell = state.magic.spells[spell_name]
+                text = spell_name + ' : ' + spell.description + ', mp cost: ' + str(spell.mp_cost) + ', spell range: ' + str(spell.range) + \
+                    ', spell power: ' + str(spell.power)
+                options.append(text)
+
+        index = self.menu.display_menu(header, options, Constants.INVENTORY_WIDTH, state.con)
+        spell_name = options[index].split(' : ')[0]
+
+        if index is None or len(self.spells) == 0:
+            Util.set_player_action(Constants.DID_NOT_TAKE_TURN)
+            return None;
+        return state.magic.spells[spell_name]
+
+    # def drop(self, object):
+    #     self.objects.append(object.owner)
+    #     self.inventory.remove(object.owner)
+    #     object.owner.x = self.player.x
+    #     object.owner.y = self.player.y
+    #     self.status_panel.message('You drop a ' + object.owner.name + '.', libtcod.turquoise)
+
+

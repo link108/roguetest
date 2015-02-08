7403f72c7e479a4ed34e9aa51b7c4bee076e6d98
Cameron Motevasselani <cmotevasselani@proofpoint.com>
2015-02-07 19:34:20 -0800

    adding some notes to the spellfile

--- lib/magic/magic.py
+++ lib/magic/magic.py
@@ -14,11 +14,12 @@ class Magic:
         with open(Constants.SPELL_FILE) as f:
             spell_file = f.readlines()
         for line in spell_file:
-            line_array = line.split(' ')
-            spell_name = line_array[0]
-            spell_stuff = line_array[1]
-            spell = Spell(spell_name, spell_stuff)
-            # spell.set_use_function(getattr(eval(spell.magic_class), spell_name))
-            self.spells[spell_name] = spell
+            if '#' not in line:
+                line_array = line.split(' ')
+                spell_name = line_array[0]
+                spell_stuff = line_array[1]
+                spell = Spell(spell_name, spell_stuff)
+                # spell.set_use_function(getattr(eval(spell.magic_class), spell_name))
+                self.spells[spell_name] = spell
 
 

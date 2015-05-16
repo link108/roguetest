708625451fc3a78d456b85379ff95cadcfb487a4
Cameron Motevasselani <cmotevasselani@proofpoint.com>
2015-05-16 14:11:53 -0700

    added time diff for map_test

--- test/map_test.py
+++ test/map_test.py
@@ -1,6 +1,7 @@
 __author__ = 'cmotevasselani'
 
 
+import datetime
 from lib.map_components.map import Map
 from lib.utility_functions.state import State
 
@@ -11,5 +12,14 @@ state = State()
 map = Map()
 state.game_map = map
 
+start_time = datetime.datetime.now()
 for i in range(100):
   map.create_map_layout(state, i)
+
+end_time = datetime.datetime.now()
+
+
+diff = end_time - start_time
+
+
+print "total time taken: " + str(diff)
